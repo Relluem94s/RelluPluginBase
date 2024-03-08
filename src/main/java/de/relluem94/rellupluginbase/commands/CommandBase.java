71c42601d0e55320082b2659dbbc80dd0d4cf902
Relluem94 <github@relluem94.de>
2024-03-09 00:04:00 +0100

    Refactor

--- src/main/java/de/relluem94/rellupluginbase/commands/CommandBase.java
+++ src/main/java/de/relluem94/rellupluginbase/commands/CommandBase.java
@@ -8,11 +8,12 @@ import org.bukkit.command.CommandSender;
 
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Groups;
 import de.relluem94.minecraft.server.spigot.essentials.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
 
 public class CommandBase implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String cmdLabel, String[] args) {
         if(cmd.getName().equalsIgnoreCase("base")){
             if(Permission.isAuthorized(sender, Groups.getGroup("user").getId())){
                 sendMessage(sender, "Drop the (Data)Base!");
--- src/main/java/de/relluem94/rellupluginbase/events/EventBase.java
+++ src/main/java/de/relluem94/rellupluginbase/events/EventBase.java
@@ -3,11 +3,12 @@ package de.relluem94.rellupluginbase.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class EventBase implements Listener {
 
     @EventHandler
-    public void playerDeath(PlayerDeathEvent e) {
-        e.setDeathMessage("§c" + e.getDeathMessage());
+    public void playerDeath(@NotNull PlayerDeathEvent event) {
+        event.setDeathMessage("§c" + event.getDeathMessage());
     }
 }
